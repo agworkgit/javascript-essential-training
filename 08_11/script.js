e0166d883645ed56fb89f02951293c8f61b6891e
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-03-01 10:02:27 +0000

    updated notes

--- 08_11/script.js
+++ 08_11/script.js
@@ -24,25 +24,18 @@ const content = `
   </figure>
   <h1 class="backpack__name">${everydayPack.name}</h1>
   <ul class="backpack__features">
-    <li class="feature backpack__volume">Volume:<span> ${
-      everydayPack.volume
-    }l</span></li>
-    <li class="feature backpack__color">Color:<span> ${
-      everydayPack.color
-    }</span></li>
+    <li class="feature backpack__volume">Volume:<span> ${everydayPack.volume
+  }l</span></li>
+    <li class="feature backpack__color">Color:<span> ${everydayPack.color
+  }</span></li>
     <li class="feature backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-    <li class="feature backpack__pockets">Number of pockets:<span> ${
-      everydayPack.pocketNum
-    }</span></li>
-    <li class="feature backpack__strap">Left strap length:<span> ${
-      everydayPack.strapLength.left
-    } inches</span></li>
-    <li class="feature backpack__strap">Right strap length:<span> ${
-      everydayPack.strapLength.right
-    } inches</span></li>
-    <li class="feature backpack__lid">Lid status:<span> ${
-      everydayPack.lidOpen
-    }</span></li>
+    <li class="feature backpack__pockets">Number of pockets:<span> ${everydayPack.pocketNum
+  }</span></li>
+    <li class="feature backpack__strap">Left strap length:<span> ${everydayPack.strapLength.left
+  } inches</span></li>
+    <li class="feature backpack__strap">Right strap length:<span> ${everydayPack.strapLength.right
+  } inches</span></li>
+    <li class="feature backpack__lid">Lid status:<span> ${everydayPack.lidOpen ? 'open' : 'closed'}</span></li>
   </ul>
 `;
 
@@ -54,3 +47,13 @@ newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
 main.append(newArticle);
+
+// Regular conditional
+if (everydayPack.lidOpen) {
+  console.log('lid is open');
+} else {
+  console.log('lid is closed');
+}
+
+// Ternary conditional
+console.log(everydayPack.lidOpen ? 'open' : 'closed');
