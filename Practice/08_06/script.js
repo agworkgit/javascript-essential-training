5e6a06325b85302c1236410acb91abf4bab9ed4a
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-03-01 08:10:31 +0000

    solved 08_06

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,27 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+// function declaration
+
+function hunger(name) {
+    console.log(`${name} is hungry.`);
+}
+
+hunger('Frank');
+
+// function expression
+
+const dogWalk = function (dog) {
+    console.log(`Taking ${dog} for a walk.`);
+}
+
+dogWalk('Flakey');
+
+// arrow function
+
+const result = ((a, b) => {
+    return a + b;
+})(2, 5);
+
+console.log(result);
